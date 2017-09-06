0498c8b6e590385c5bd17ad87d67dd5f6c3866c5
pesnewupdate <31643111+pesnewupdate@users.noreply.github.com>
2017-09-06 10:40:34 +0700

    Update safelinkv2.js

--- safelinkv2.js
+++ safelinkv2.js
@@ -19,11 +19,13 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*3)+ 1);
+var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="home-based-internet-marketing-business.html"}
-if(x=="2"){xxx="how-to-succeed-in-a-though-home-business-world.html"}
-if(x=="3"){xxx="tips-for-running-successful-home-business.html"}a_to_ve[i].href="http://www.pesnewupdate.click/2017/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="strategies-for-success-in-a-home-business.html"}
+if(x=="2"){xxx="starting-home-business-that-can-make-you-a-profit.html"}
+if(x=="3"){xxx="home-based-internet-marketing-business.html"}
+if(x=="4"){xxx="how-to-succeed-in-a-though-home-business-world.html"}
+if(x=="5"){xxx="tips-for-running-successful-home-business.html"}a_to_ve[i].href="http://www.pesnewupdate.click/2017/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
